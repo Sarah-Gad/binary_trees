a602d82a9342b97efb2b451612729d0471ee8647
Sarah-Gad <sarahgad298@gmail.com>
2024-03-28 05:14:09 -0700

    completd task n.1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - I used this function to add a a left child node
+ * @parent: ptr to the node that is supposed to be the parent
+ * of the newly created node.
+ * @value: the integer to beincluded in the new node.
+ * Return: it will ewturn a ptr to the newly created nd.
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+	binary_tree_t *nw_cre_nd = malloc(sizeof(binary_tree_t));
+
+	if (nw_cre_nd == NULL)
+		return (NULL);
+	if (parent->left == NULL)
+	{
+		parent->left = nw_cre_nd;
+		nw_cre_nd->n = value;
+		nw_cre_nd->parent = parent;
+		nw_cre_nd->left = NULL;
+		nw_cre_nd->right = NULL;
+	}
+	else
+	{
+		nw_cre_nd->n = value;
+		nw_cre_nd->parent = parent;
+		nw_cre_nd->left = parent->left;
+		nw_cre_nd->right = NULL;
+		parent->left->parent = nw_cre_nd;
+		parent->left = nw_cre_nd;
+	}
+	return (nw_cre_nd);
+}
+
