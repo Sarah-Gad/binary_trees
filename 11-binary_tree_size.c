fbaedbd337e3c0840e00b3794d0a39fa1f158bfc
Sarah-Gad <sarahgad298@gmail.com>
2024-03-28 18:10:45 -0700

    edit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,7 +9,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t sz_int = 0;
 
 	if (tree == NULL)
-		return (0);
+		return (sz_int);
 	sz_int += 1;
 	binary_tree_size(tree->left);
 	binary_tree_size(tree->right);
